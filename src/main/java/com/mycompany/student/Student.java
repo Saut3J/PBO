bc40e5c541c461827479df10111206ce1adc8b6c
mikom <mikom@Saut>
2026-03-06 15:51:47 +0700

    fourth commit

--- src/main/java/com/mycompany/student/Student.java
+++ src/main/java/com/mycompany/student/Student.java
@@ -1,16 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.student;
 
-/**
- *
- * @author mikom
- */
 public class Student {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private String studentId;
+    private String studentName;
+    private double score1;
+    private double score2;
+    private double score3;
+
+    public Student() {
+    }
+
+    public Student(String studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public void setScore1(double score1) {
+        if (score1 >= 0 && score1 <= 100) {
+            this.score1 = score1;
+        } else {
+            System.out.println("Nilai 1 harus antara 0 - 100");
+        }
+    }
+
+    public void setScore2(double score2) {
+        if (score2 >= 0 && score2 <= 100) {
+            this.score2 = score2;
+        } else {
+            System.out.println("Nilai 2 harus antara 0 - 100");
+        }
+    }
+
+    public void setScore3(double score3) {
+        if (score3 >= 0 && score3 <= 100) {
+            this.score3 = score3;
+        } else {
+            System.out.println("Nilai 3 harus antara 0 - 100");
+        }
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public double calculateAverage() {
+        return (score1 + score2 + score3) / 3;
     }
 }
