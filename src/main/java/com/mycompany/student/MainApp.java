1f2c6ce02bcb296ee1a1c3b146abb93ca0f84612
mikom <mikom@Saut>
2026-03-06 15:53:32 +0700

    fifth commit

--- src/main/java/com/mycompany/student/MainApp.java
+++ src/main/java/com/mycompany/student/MainApp.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.student;
 
-/**
- *
- * @author mikom
- */
+import java.util.Scanner;
+
 public class MainApp {
-    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== SISTEM NILAI MAHASISWA ===");
+
+        System.out.print("Masukkan Nama Mahasiswa: ");
+        String nama = input.nextLine();
+
+        System.out.print("Masukkan NIM Mahasiswa: ");
+        String id = input.nextLine();
+
+        Student student = new Student(id, nama);
+
+        System.out.print("Masukkan Nilai 1: ");
+        double n1 = input.nextDouble();
+        student.setScore1(n1);
+
+        System.out.print("Masukkan Nilai 2: ");
+        double n2 = input.nextDouble();
+        student.setScore2(n2);
+
+        System.out.print("Masukkan Nilai 3: ");
+        double n3 = input.nextDouble();
+        student.setScore3(n3);
+
+        GradeService service = new GradeService();
+        service.printResult(student);
+    }
 }
